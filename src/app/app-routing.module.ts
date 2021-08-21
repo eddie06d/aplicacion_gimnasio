44d95110ccb097f873cb8de5c33a8763a913071d
Palacios <bryanpalaciosg8@gmail.com>
2021-08-21 15:49:54 -0500

    agregue el registro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { BodyComponent } from './components/body/body.component';
 import { LoginComponent } from './components/login/login.component';
+import { RegistroComponent } from './components/registro/registro.component';
 
 const routes: Routes =[
   {path: '', component: BodyComponent},
-  {path: 'login', component: LoginComponent}
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegistroComponent}
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
